0b477018d388ef83770efc7fbc5238d6de4df2b5
ByunDohwi <dohwe0978@gmail.com>
2024-10-13 14:35:37 +0900

    add :: admin entity

--- src/main/kotlin/pick_landing_server/domain/user/admin/domain/Admin.kt
+++ src/main/kotlin/pick_landing_server/domain/user/admin/domain/Admin.kt
@@ -1,4 +1,15 @@
 package pick_landing_server.domain.user.admin.domain
 
-class Admin {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+data class Admin (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long,
+    private val accountId: String,
+    private val password: String
+)
