0d06d86131162ead98e859df8aa27e6c5303e91c
ByunDohwi <dohwe0978@gmail.com>
2024-10-13 14:36:09 +0900

    add :: admin repository

--- src/main/kotlin/pick_landing_server/domain/user/admin/domain/repository/AdminRepository.kt
+++ src/main/kotlin/pick_landing_server/domain/user/admin/domain/repository/AdminRepository.kt
@@ -1,4 +1,10 @@
 package pick_landing_server.domain.user.admin.domain.repository
 
-class AdminRepository {
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import pick_landing_server.domain.user.admin.domain.Admin
+
+@Repository
+interface AdminRepository : CrudRepository<Admin,Long> {
+    fun findByAccountId(accountId:String)
 }
