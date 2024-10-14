79db000f5515ecd01b81829782c3a31b74eb2396
ByunDohwi <dohwe0978@gmail.com>
2024-10-14 14:39:33 +0900

    add :: test controller

--- src/main/kotlin/pick_landing_server/domain/TestController.kt
+++ src/main/kotlin/pick_landing_server/domain/TestController.kt
@@ -1,12 +1,20 @@
 package pick_landing_server.domain
 
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pick_landing_server.domain.auth.domain.repository.RefreshTokenRepository
+import pick_landing_server.global.security.jwt.exception.InvalidJwtException
 
 @RestController
-@RequestMapping("test")
-class TestController {
-    @GetMapping("/haha")
-    fun haha():String ="haha"
+@RequestMapping("/test")
+class TestController(
+    private val refreshTokenRepository: RefreshTokenRepository
+) {
+    @GetMapping("/haha/{refresh}")
+    fun haha(@PathVariable refresh:String): String {
+       return refreshTokenRepository.findByToken(refresh)?.token ?: throw InvalidJwtException
+   }
 }
