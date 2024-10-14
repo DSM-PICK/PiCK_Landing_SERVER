c740c3c39fdd0321382da36d10596e8fb2e52023
ByunDohwi <dohwe0978@gmail.com>
2024-10-14 14:37:59 +0900

    fix :: already exist admin 클래스 이름 수정

--- src/main/kotlin/pick_landing_server/domain/user/admin/exception/AlreadyExistAdmin.kt
+++ src/main/kotlin/pick_landing_server/domain/user/admin/exception/AlreadyExistAdmin.kt
@@ -3,6 +3,6 @@ package pick_landing_server.domain.user.admin.exception
 import pick_landing_server.global.error.exception.ErrorCode
 import pick_landing_server.global.error.exception.PickException
 
-object AlreadyExistAdmin :PickException(
+object AlreadyExistAdminException :PickException(
     ErrorCode.ALREADY_EXIST_ADMIN
 )
