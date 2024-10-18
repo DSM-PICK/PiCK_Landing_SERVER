3f9e1b8c6c783b832f2e2f885f60c2a0c8fe146b
imhanseong <hans_hanseong@dsm.hs.kr>
2024-10-18 17:18:19 +0900

    add :: application request

---
+++ src/main/kotlin/pick_landing_server/domain/application/presentation/dto/ApplicationRequest.kt
@@ -0,0 +1,21 @@
+package pick_landing_server.domain.application.presentation.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.Length
+import pick_landing_server.domain.application.domain.Major
+
+data class ApplicationRequest (
+    @field:NotBlank
+    val name: String,
+    @field:NotNull(message = "공백 안됨")
+    val schoolNum: Int,
+    @field:NotBlank(message = "phone_number은 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
+    @Length(max = 11)
+    val phone: String,
+    @field:NotNull
+    val major: Major,
+    @field:NotNull @Size(max = 300, message = "300글자 내외로 작성해주세요")
+    val motivation: String
+)
