70756904a3288166b7533a79c65430e60c9f5830
imhanseong <hans_hanseong@dsm.hs.kr>
2024-10-18 17:21:45 +0900

    add :: validationException

--- src/main/kotlin/pick_landing_server/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/pick_landing_server/global/error/GlobalExceptionHandler.kt
@@ -1,8 +1,10 @@
 package pick_landing_server.global.error
 
+import jakarta.validation.ValidationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import pick_landing_server.global.error.exception.PickException
@@ -15,6 +17,17 @@ class GlobalExceptionHandler() {
         return ResponseEntity(
             ErrorReponse(code.status, code.message),
             HttpStatus.valueOf(code.status)
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun validationException(e: MethodArgumentNotValidException): ResponseEntity<ErrorReponse> {
+        return ResponseEntity(
+            ErrorReponse(
+                400,
+                e.bindingResult.allErrors[0].defaultMessage
+            ),
+            HttpStatus.BAD_REQUEST
         )
     }
 }
