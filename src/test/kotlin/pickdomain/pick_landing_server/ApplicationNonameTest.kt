e672277962f074f8627666084d996107611c4f2b
imhanseong <hans_hanseong@dsm.hs.kr>
2024-10-18 17:19:35 +0900

    add :: 지원 실패 테스트 코드 추가

---
+++ src/test/kotlin/pickdomain/pick_landing_server/ApplicationNonameTest.kt
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import jakarta.validation.ConstraintViolationException
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import pick_landing_server.domain.application.domain.Major
+import pick_landing_server.domain.application.presentation.dto.ApplicationRequest
+import pick_landing_server.domain.application.service.ApplicationService
+
+@SpringBootTest(classes = [ApplicationService::class])
+class ApplicationNonameTest {
+    @MockBean
+    private lateinit var applicationService: ApplicationService
+    private val schoolNum = 1214
+    private val phoneNum = "010-1234-1234"
+    private val major = Major.BackEnd
+    private val motivation = "안녕하세요"
+
+    @Test
+    fun applicationNoname() {
+        val request = ApplicationRequest(
+            name = "",
+            schoolNum = schoolNum,
+            phone = phoneNum,
+            major = major,
+            motivation = motivation
+        )
+
+        assertThrows(ConstraintViolationException::class.java) {
+            applicationService.execute(request)
+        }
+    }
+}
