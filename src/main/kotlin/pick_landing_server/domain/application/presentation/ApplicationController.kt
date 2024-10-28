9b52f69fcaff8394df7891a41040ce04586d9b5b
dohwi <dohwe0978@gmail.com>
2024-10-28 23:32:02 +0900

    add :: application 조회

--- src/main/kotlin/pick_landing_server/domain/application/presentation/ApplicationController.kt
+++ src/main/kotlin/pick_landing_server/domain/application/presentation/ApplicationController.kt
@@ -3,15 +3,23 @@ package pick_landing_server.domain.application.presentation
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
 import pick_landing_server.domain.application.presentation.dto.ApplicationRequest
+import pick_landing_server.domain.application.presentation.dto.QueryApplicationResponse
 import pick_landing_server.domain.application.service.ApplicationService
+import pick_landing_server.domain.application.service.QueryApplicationService
 
 @RestController
 @RequestMapping("/application")
 class ApplicationController (
-    private val applicationService: ApplicationService
+    private val applicationService: ApplicationService,
+    private val queryApplicationService: QueryApplicationService
 ){
     @PostMapping
     fun application(@Valid @RequestBody applicationRequest: ApplicationRequest){
         applicationService.execute(applicationRequest)
+    }
+
+    @GetMapping
+    fun queryApp():List<QueryApplicationResponse> {
+        return queryApplicationService.queryApplication();
     }
 }
